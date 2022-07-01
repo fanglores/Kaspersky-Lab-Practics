f69c6a92f2aed045521d1f048cf957fbf9ffa177
Konstantin <72685417+fanglores@users.noreply.github.com>
2022-07-01 17:36:30 +0300

    Add files via upload

--- source_files/linux_server/main.py
+++ source_files/linux_server/main.py
@@ -4,9 +4,15 @@ from sockets_module import *
 if __name__ == '__main__':
     print('[DEBUG] Launching...')
 
+    # init basic entities
     RecSys = RecognitionUnit()
     SocExc = TCPUnit()
 
-    RecSys.show_image(RecSys.get_image('blue'))
+    # test preview
+    cv2.imshow('Test', RecSys.get_image())
 
+    # stop program if window was closed or q was pressed
+    while (not cv2.waitKey(50) & 0xFF == ord('q')) and (cv2.getWindowProperty('Test', cv2.WND_PROP_VISIBLE) >= 1):
+        pass
 
+    print('[DEBUG] Program exit')
--- source_files/linux_server/recognition_module.py
+++ source_files/linux_server/recognition_module.py
@@ -1,39 +1,77 @@
 import cv2
+import numpy as np
 
 
 class RecognitionUnit:
 
     def __init__(self):
         try:
+            # init camera entity
             self.Camera = cv2.VideoCapture(0)
+            # takes too long
+            # crop capture?
+
             if not self.Camera.isOpened():
                 raise 'Can`t open the video stream'
 
-            # masks init
+            # define masks boundaries
+            self.red_lower = np.array([136, 87, 111], np.uint8)
+            self.red_upper = np.array([180, 255, 255], np.uint8)
+
+            self.green_lower = np.array([25, 52, 72], np.uint8)
+            self.green_upper = np.array([102, 255, 255], np.uint8)
+
+            self.blue_lower = np.array([94, 80, 2], np.uint8)
+            self.blue_upper = np.array([120, 255, 255], np.uint8)
 
             print('[DEBUG] RecognitionUnit initialisation successful!')
         except:
             print('[ERROR] Error while initialising RecognitionUnit!')
 
-    def get_image(self, mask=''):
+    def __del__(self):
+        self.Camera.release()
+        cv2.destroyAllWindows()
+
+    def run(self):
+        self.get_contours(self.get_image())
+
+        coords = []
+
+        return coords
+
+    def get_image(self):
         try:
             ret, frame = self.Camera.read()
 
             if ret:
-                if mask == '':
-                    return frame
-                if mask == 'blue':
-                    return frame  # +blue mask
-
-                raise 'No mask'
+                return frame
             else:
                 raise 'Retrieving error'
         except:
             print('[ERROR] Error while retrieving an image!')
 
-    def show_image(self):
-        try:
-            while True:
-                cv2.imshow('Current frame', self.get_image())
-        except:
-            print('[ERROR] Error while displaying an image!')
+    def get_contours(self, frame):
+        hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        red_mask = cv2.inRange(hsvFrame, self.red_lower, self.red_upper)
+        green_mask = cv2.inRange(hsvFrame, self.green_lower, self.green_upper)
+        blue_mask = cv2.inRange(hsvFrame, self.blue_lower, self.blue_upper)
+
+        # remove  noise
+        kernel = np.ones((5, 5), "uint8")
+
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, kernel)
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+
+        green_mask = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, kernel)
+        green_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+
+        blue_mask = cv2.morphologyEx(red_mask, cv2.MORPH_CLOSE, kernel)
+        blue_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+
+        # find contours
+        red_contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        green_contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        return [red_contours, green_contours, blue_contours]
